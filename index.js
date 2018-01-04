2ad21e2cb3b90ec545246118d46c707334d96e0a
Victor Sandoval <victor.sandoval@orbisa.com.pe>
2018-01-04 18:10:13 -0500

    refactor(critical): se reestructura compilacion de critical css

--- index.js
+++ index.js
@@ -2,61 +2,63 @@ const puppeteer = require("puppeteer");
 const critical  = require("critical");
 const fs        = require("fs");
 const config    = require("./config");
-
+const pathFiles = "./";
 //const pathFiles = require("/usr/local/app/frontend/config/docker-path.js");
 
-async function scrape (uri) {
-  // Init browser
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+let initBrowser = function() {
+   return puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+};
+
+let getPage = async function (url, browser) {
   const page = await browser.newPage();
+  await page.goto(`${url}`, { timeout: 120000 });
+  return page.content();
+};
 
-  // Get body content
-  console.info(`Plugin critical: Init! ${config.host}/${uri}`);
-  await page.goto(`${config.host}/${uri}`, {timeout: 120000});
-  const body = await page.content();
+let getCssCollection = function (browser, content) {
+  if (!content) return;
 
-  // Get css content
-  let cssUri = body.match(/https?:\/\/(cds|cdn)\.[a-z_\-0-9\.\/]+(?<!ie)\.css\?v=([a-z0-9]+)?/g);
+  let cssUri = content.match(/https?:\/\/(cds|cdn)\.[a-z_\-0-9\.\/]+(?<!ie)\.css\?v=([a-z0-9]+)?/g);
   if (!cssUri) {
-    console.error("Plugin critical: cssUri is null");
+    console.error("cssUri is null");
     browser.close();
     return;
   }
-  console.log(cssUri)
+  return cssUri;
+};
 
+let getContentCSS = async function (browser, cssUrls) {
   let cssContent = "";
-  for (let url of cssUri) {
-    await page.goto(url);
-    const cssContentTmp = await page.content();
-    const regToDeleteTags = /(<([^>]+)>)/ig;
-    const justCss = cssContentTmp.replace(regToDeleteTags, "");
+
+  for (let url of cssUrls) {
+    let contentCSS = await getPage(url, browser);
+    const detectTags = /(<([^>]+)>)/ig;
+    const justCss = contentCSS.replace(detectTags, "");
     cssContent = cssContent + justCss;
   }
+
   browser.close();
-  return {
-    body,
-    cssContent
-  }
-}
+  return cssContent;
+};
 
-function generateCritical(uri, file, scrapedData) {
-  const fileTempName = `tmp${uri}${new Date().getTime()}.css`;
-
-  if (scrapedData.cssContent === "") {
-    console.info(`${config.host}/${uri} doesn't have CSS :C`);
+let generateTempFiles = function (url, fileName, cssContent) {
+  if (cssContent === "") {
+    console.info(`${url} doesn't have CSS :C`);
     return;
   }
 
-  // Generate temporal css file
   try {
-    let content = scrapedData.cssContent.replace(/&gt;/g, ">");
-    fs.writeFileSync(fileTempName, content);
+    let content = cssContent.replace(/&gt;/g, ">");
+    fs.writeFileSync(fileName, content);
   } catch (e) {
-    console.error(`Plugin critical: Cannot write the temporal file ${fileTempName} for ${config.host}/${uri}: ${e.message}`);
+    console.error(`Cannot write the temporal file ${fileName} for ${url} - ${e.message}`);
   }
+};
 
+let generateCritical = function (url, fileTempName, contentBody) {
+  console.info(`Generating critical for ${url} ... `);
   critical.generate({
-    html      : scrapedData.body,
+    html      : contentBody,
     folder    : "dist/",
     dimensions: [
       { height: 736, width : 414  },
@@ -74,32 +76,39 @@ function generateCritical(uri, file, scrapedData) {
     try {
       fs.unlinkSync(fileTempName);
     } catch (e) {
-      console.error(`Plugin critical: Cannot delete the temporal file ${fileTempName} for ${config.host}/${uri}: ${e.message}`);
+      console.error(`Cannot delete the temporal file ${fileTempName} for ${url} - ${e.message}`);
     }
 
     // Template css inline
     const dataWithCritical = `if locals.environment !== "local"\n  | <style>${output}</style>`;
 
     // Write .jade file with dataWithCritical
-    /*try {
-      fs.writeFileSync(`${pathFiles.output.critical}${file}/critical.jade`, dataWithCritical);
-      console.info(`Plugin critical: Success! ${config.host}/${uri}`);
+    try {
+      fs.writeFileSync(`${pathFiles}/${fileTempName}.jade`, dataWithCritical);
+      console.info(`Success! ${url}`);
     } catch (e) {
-      console.error(`Plugin critical: Cannot write file ${pathFiles.output.critical}${file}/critical.jade: ${e.message}`);
-    }*/
+      console.error(`Cannot write file critical.jade: ${e.message}`);
+    }
   }).error(function (e) {
-    console.error(`Plugin critical: Something wrong with critical :C ! ${e.message}`);
+    console.error(`Something wrong with critical :C ! ${e.message}`);
   });
-}
+};
 
 function main (config) {
   config.pages.map(async (page) => {
-    scrape(page.uri)
-      .then((scrapedData) => {
-        // generateCritical(page.uri, page.fileRoute, scrapedData);
-      }).catch((e) => {
-          console.error(`Plugin critical: Error in loading ${config.host}/${page.uri}: ${e.message}`);
-      })
+    const url = `${config.host}/${page.uri}`;
+    const fileTempName = `tmp${page.uri}${new Date().getTime()}.css`;
+
+    console.info(`Initialize page ${url}`);
+    let browser = await initBrowser();
+    let contentBody = await getPage(url, browser)
+      .catch((e) => console.error(e.message));
+    let cssUrls = getCssCollection(browser, contentBody);
+    let cssContent = await getContentCSS(browser, cssUrls)
+      .catch((e) => console.error(e.message));
+
+    generateTempFiles(url, fileTempName, cssContent);
+    generateCritical(url, fileTempName, contentBody);
   });
 }
 
